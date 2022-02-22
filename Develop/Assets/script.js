2e8d8f4ec06fd2e221fb46426aa22f147fa01e7d
bvsswave <tannershahan@gmail.com>
2022-02-21 20:00:03 -0700

    js is now working properly

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -16,57 +16,67 @@
 
 // Variables
 
-var saveBtn = $(".saveBtn");
+ var saveBtn = $(".saveBtn");
 
 // Functions
 
+ 
 // THEN the current day is displayed at the top of the calendar
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
-
-// color coded to indicate if it is in the past, present, or future
-function timeBlockColor() {
-    var hour = moment().hours();
-
-    $(".time-block").each(function() {
-        // current hour
-        var currHour = parseInt($(this).attr("id"));
-
-        // console.log(this)
-
-        if (currHour > hour) {
-            $(this).addClass("future");
-        } else if (currHour === hour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("past");
-        }
-    })
-};
-
+ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+ 
+ // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+ function timeBlockColor() {
+     var hour = moment().hours();
+ 
+     $(".time-block").each(function() {
+         // var currHour = parseInt($(this).attr("id"));
+          var currHour = parseInt($(this).attr("id"));
+ 
+         // console.log(this); //each time-block
+ 
+         if (currHour > hour) {
+             $(this).addClass("future");
+         } else if (currHour === hour) {
+             $(this).addClass("present");
+         } else {
+             $(this).addClass("past");
+         }
+     })
+ };
+ 
 // WHEN I click into a time block
-saveBtn.on("click", function() {
+ saveBtn.on("click", function() {
+ 
+     // console.log(this); //save button
+     var time = $(this).siblings(".hour").text();
+     var plan = $(this).siblings(".plan").val();
+ 
+     // THEN the text for that event is saved in local storage
+     localStorage.setItem(time, plan);
+ });
+ 
+ // WHEN I refresh the page
+ // THEN the saved events persist
+ function usePlanner() {
+ 
+     $(".hour").each(function() {
+         var currHour = $(this).text();
+         var currPlan = localStorage.getItem(currHour);
+ 
+         // console.log(this);
+         // console.log(currHour);
+ 
+         if(currPlan !== null) {
+             $(this).siblings(".plan").val(currPlan);
+         }
+     });
+ }
 
-    var time = $(this).siblings(".hour").text();
-    var plan = $(this).sibling("plan").val();
-    
-// THEN I can enter an event
-    localStorage.setItem(time, plan);  
-});
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
-function usePlanner() {
-    $(".hour").each(function() {
-        var currHour = $(this).text();
-        var currPlan = localStorage.getItem(currHour);
-
-        if(currPlan !== null) {
-            $(this).siblings(".plan").val(currPlan);
-        }
-    });
-}
-
-
-timeBlockColor();
-usePlanner();
+//  CALL FUNCTIONS
+ 
+ timeBlockColor();
+ usePlanner();
+ 
+ 
+ 
+ 
